77553d4cd4bb9ee9a9d17151ea20506c04e119d8
kittu-54545 <71482997+kittu-54545@users.noreply.github.com>
2026-07-26 14:03:58 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        // tabulation bottom up
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+        int pre1 = nums[0], pre2 = max(nums[0], nums[1]), ans;
+        for (int i = 2; i < n; i++) {
+            int take = nums[i] + pre1;
+            int notake = pre2;
+            ans = max(take, notake);
+            pre1 = pre2;
+            pre2 = ans;
+        }
+        return ans;
+    }
+};
